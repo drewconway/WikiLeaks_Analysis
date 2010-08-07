bbb1cce78a6a8b7d2ba14e0d263b02cf098e59b2
Drew Conway <agconway@gmail.com>
2010-08-07 09:35:15 -0400

    Added map of Afghanistan to Mike's code

--- wikileaks_TS.r
+++ wikileaks_TS.r
@@ -1,5 +1,6 @@
 
 library(ggplot2)
+library(maptools)
 
 afg <- read.csv("afg.csv",stringsAsFactors=FALSE)
 
@@ -18,11 +19,20 @@ afg_ts <- data.frame(
     label = factor(apply(cbind(afg$AttackOn,afg$Type),1,function(row)paste(row[1],row[2],sep="_")))
 )
 
+# Read Afghanistan shapefile
+temp <- tempfile()
+download.file("http://www.aims.org.af/services/mapping/shape_files/afghanistan/polygon/district_boundary.zip",temp)
+untar(temp,compressed="gzip")
+unlink(temp)
+afg.shp <- readShapePoly("admin3_poly_32.shp")
+afg.poly<-fortify.SpatialPolygons(afg.shp)
+
+
 # rough lat/lon limits of Afghanistan:
-max_lat = 37.3
-min_lat = 29.4
-max_lon = 75.0
-min_lon = 60.6
+max_lat = max(afg.poly$lat)
+min_lat = min(afg.poly$lat)
+max_lon = max(afg.poly$lon)
+min_lon = min(afg.poly$lon)
 
 # filter out events not in Afghanistan (?!)
 to_keep <- (afg_ts$lat > min_lat) & (afg_ts$lat < max_lat) & (afg_ts$lon > min_lon) & (afg_ts$lon < max_lon)
@@ -33,7 +43,8 @@ to_throw <- is.na(afg_ts$label)
 afg_ts <- afg_ts[!to_throw,]
 afg_ts$label = factor(as.character(afg_ts$label))
 
-png("images/events_by_label.png",width=1200,height=750,res=100)
-p <- ggplot(afg_ts,aes(x=lat,y=lon))
-p + geom_point(aes(color=label,alpha=0.4)) + facet_wrap(~ year)
+png("images/events_by_label_map.png",width=1200,height=750,res=100)
+p <- ggplot(afg_ts,aes(x=lat,y=long)) + geom_point(aes(x=lat,y=lon,color=label,alpha=0.4)) +facet_wrap(~year)
+p <-ggpath(p,data=afg.poly aes=list(x=lat,y=lon,group=group))
+print(p)
 dev.off()
